8f2ef822dd5e415527cfc7339fa4cd25cab1b3d6
kjacasio <59855973+kjacasio@users.noreply.github.com>
2020-02-11 12:49:52 -1000

    completed problem sets for Hackwich 3 Feb 11

--- Hackwich3/ViewController.swift
+++ Hackwich3/ViewController.swift
@@ -16,14 +16,20 @@ var secondString = "The background color will turn to green"
     
     @IBOutlet weak var RedLabel: UILabel!
     @IBOutlet weak var BlueLabel: UILabel!
+    @IBOutlet weak var GreenLabel: UILabel!
+    
+    var backgroundString = "The background will turn red"
+    var foregroundString = "The background color will turn to green"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.}
+    }
 
-
-        func changeButtonPressed(_ sender: Any)
-    {
+//solution problem set #1
+        
+    @IBAction func changeColorButtonPressed(_ sender: Any) {
+        
         if firstString == "The background color will turn red"
         {
             self.view.backgroundColor = UIColor.red
@@ -34,31 +40,37 @@ var secondString = "The background color will turn to green"
             self.view.backgroundColor = UIColor.blue
             self.BlueLabel.text="Blue"
         }
+    }
     
-        class ViewController: UIViewController {
-            
-    //Declare variables here
-        
-            @IBOutlet weak var GreenLabel: UILabel!
-            
-            override func viewDidLoad() {
-                super.viewDidLoad()
-                // Do any additional setup after loading the view.}
-            }
-      
-var backgroundString = "The background will turn red"
-var foregroundString = "The background color will turn to green"
+    //solution for Problem set #2
 
-            @IBAction func changeButtonPressed(_ sender: Any)
-        {
-            if self.backgroundString == "The background color will turn green"
-            {
-                self.view.backgroundColor = UIColor.green
-                self.GreenLabel.text="Green"
-            }
-            print ("I completed both problem sets")
+ 
+    @IBAction func magicButtonPressed(_ sender: Any)
+    {
+        
+        //code block
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
-}
-}
-}
-}
+            
+            
+
